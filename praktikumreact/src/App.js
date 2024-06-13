be18dc5daec5a03297b2ee7e7a8c5f3d5a4f0ea4
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-06-13 23:04:13 +0700

    first commit

---
+++ praktikumreact/src/App.js
@@ -0,0 +1,18 @@
+import FAUZAN from './FAUZAN.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={FAUZAN} className="App-logo" alt="logo" />
+        <p>
+          AGUNG ADALAH ANAK YANG RAJIN BELAJAR.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
